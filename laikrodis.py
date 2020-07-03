a371f87a91507d43e8abd87de267d25ba014e16b
Valdas1975 <k.valdas@protonmail.com>
2020-07-03 15:41:14 +0300

    Initial commit

---
+++ laikrodis.py
@@ -0,0 +1,17 @@
+from tkinter import*
+import time
+
+def tick():
+    time_string = time.strftime("%H:%M:%S")
+    clock.config(text=time_string)
+    clock.after(200,tick)
+
+root =Tk ()
+root.title('LAIKRODIS')
+clock = Label(root, font =("times", 50,"bold"),bg="#999999")
+clock.grid(row=0,column=1)
+tick()
+root.mainloop()
+
+
+
